6782e1dce836b7999d72f42889be13fce982db0f
oscar <omatalla@LAPTOP-ULR86CR3.localdomain>
2026-06-10 18:04:48 +0200

    updated main_GNL.c, added main to test the standard input and more test files

--- standard_input.c
+++ standard_input.c
@@ -1,52 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main_GNL.c                                         :+:      :+:    :+:   */
+/*   standard_input.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: omatalla <omatalla@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/06/10 15:56:52 by omatalla          #+#    #+#             */
-/*   Updated: 2026/06/10 17:51:07 by omatalla         ###   ########.fr       */
+/*   Created: 2026/06/10 17:51:34 by omatalla          #+#    #+#             */
+/*   Updated: 2026/06/10 17:58:08 by omatalla         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include "get_next_line.h"
 
 int	main(void)
 {
-	int		fd;
 	char	*line;
 
-	fd = open("14. Test_Oscar.txt", O_RDONLY);
-	if (fd < 0)
-		return (1);
-	line = get_next_line(fd);
-	while (line)
-	{
-		printf("[%s]", line);
-		free(line);
-		line = get_next_line(fd);
-	}
-	printf("\n");
-	close(fd);
+	printf("Write input:\n");
+	line = get_next_line(0);
+	printf("Read: [%s]", line);
+	free(line);
 	return (0);
 }
-/*List of files:
-01. A.txt
-02. About_get_next_line.txt
-03. An.txt
-04. AnBn.txt
-05. Coding_get_next_line.txt
-06. Empty.txt
-07. Everything_Emptiness_Poem.txt
-08. n.txt
-09. nnn.txt
-10. Random.txt
-11. Videos_get_next_line.txt
-12. HW42.txt
-13. Long_Text.txt
-14. Test_Oscar.txt
-*/
