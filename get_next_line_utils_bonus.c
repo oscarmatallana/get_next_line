31ee5b0c2e45deaf578f9c892d318466aa1368e3
oscar <omatalla@LAPTOP-ULR86CR3.localdomain>
2026-06-11 14:34:51 +0200

    updated testers file and header in utils for bonus

--- get_next_line_utils_bonus.c
+++ get_next_line_utils_bonus.c
@@ -1,16 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*   get_next_line_utils_bonus.c                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: omatalla <omatalla@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/09 16:15:15 by omatalla          #+#    #+#             */
-/*   Updated: 2026/06/09 18:30:18 by omatalla         ###   ########.fr       */
+/*   Updated: 2026/06/11 14:28:33 by omatalla         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line.h"
+#include "get_next_line_bonus.h"
 
 char	*ft_strdup(const char *s)
 {
